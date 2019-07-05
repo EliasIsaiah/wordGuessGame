0f3344df786de3502ffe9a760c64d2694333f675
Elias Roussos <eliasisaiah@outlook.com>
2019-07-04 23:21:56 -0500

    -first (undoubtedly broken) attempt at word guessing game

--- Assets/js/game.js
+++ Assets/js/game.js
@@ -1,3 +1,15 @@
 window.onload = function() {
-    var 
+    
+    let words = [mountain, waterfall, boulder, goat, sheep, glacier, alpaca, trail, tree, snowfall, volcano, vista, hiking, underbrush, foliage, fauna];
+    
+    let randomWord = words[Math.floor(Math.random()*myArray.length)];
+    
+    console.log( randomWord );
+
+    let rwordLength = randomWord.length;
+
+    for( let i = 0; i < rwordLength; i++ )
+    {
+        document.getElementsByClassName('content').innerHTML += "_ ";
+    }
 }
