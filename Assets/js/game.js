5890c0b1af72d6511be70331dd439b8467cbd8c6
Elias Roussos <eliasisaiah@outlook.com>
2019-07-04 23:27:35 -0500

    -updated js to point to instantiated array object instead of the placeholder name from the boilerplate

--- Assets/js/game.js
+++ Assets/js/game.js
@@ -2,7 +2,7 @@ window.onload = function() {
     
     let words = ['mountain', 'waterfall', 'boulder', 'goat', 'sheep', 'glacier', 'alpaca', 'trail', 'tree', 'snowfall', 'volcano', 'vista', 'hiking', 'underbrush', 'foliage', 'fauna'];
     
-    let randomWord = words[Math.floor(Math.random()*myArray.length)];
+    let randomWord = words[Math.floor(Math.random()*words.length)];
     
     console.log( randomWord );
 
