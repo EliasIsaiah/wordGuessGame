11f98354edf79e89005db2b6cd1dc7b120c74a46
Elias Roussos <eliasisaiah@outlook.com>
2019-07-10 18:26:49 -0400

    -another js file created for testing purposes

---
+++ Assets/js/game3.js
@@ -0,0 +1,7 @@
+let game = {
+    words: ["mountain", "waterfall", "boulder", "goat", "sheep", "glacier", "alpaca", "trail", "tree", "snowfall", "volcano", "vista", "hiking", "underbrush", "foliage", "fauna"],
+    $randomWord: "",
+    $rWordLength: 0,
+    $wordGuess: "",
+    $input: "",
+}
